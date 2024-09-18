b23e69284e7c024d1f432de7e155465b9c8cc5e7
Shounak Desai <sd9649@rit.edu>
2024-09-18 12:36:24 -0400

    First Commit

---
+++ snn_testing/main.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+import numpy as np
+import matplotlib.pyplot as plt
+import torchvision
+import mlflow
+from vanilla_snn import StochasticNN
+import pandas as pd
+import os
+from torch import optim 
+
+EPOCHS = 100
+LR_RATE = 0.01
+
+INPUT_DIM = 4     
+OUTPUT_DIM = 3
+
+def import_data():
+    """
+    This function is used to import the training data.
+    :return: X: X1 and X2 features of the dataset which becomes our input variable for the model.
+             Y: Y is our target variable.
+    """
+    path = os.getcwd() + '/snn_testing/data/iris_train.dat'
+    data = pd.read_csv(path, header=None, names=['X1', 'X2', 'X3', 'X4', 'Y'])
+    X = data[['X1', 'X2', 'X3', 'X4']].to_numpy()
+    Y = np.array(data['Y'])
+
+    return torch.Tensor(X), torch.Tensor(Y)
+
+def one_hot_encode(Y):
+    """
+    performing one hot encoding over target variable Y
+    :param Y:
+    :return: one hot encoded Y
+    """
+    num_classes_k = len(np.unique(Y))
+    new_y = np.zeros((Y.shape[0], num_classes_k))
+    for i in range(len(Y)):
+        if Y[i] == 0:
+            new_y[i][0] = 1
+        else:
+            new_y[i][1] = 1
+
+    return new_y
+
+
+def train(snn, x, y):
+    # Define loss function and optimizer
+    loss_fn = nn.CrossEntropyLoss()  # since this is a classification problem
+    optimizer = optim.Adam(snn.parameters(), lr=LR_RATE)
+
+    snn.train()  # put the model in training mode
+
+    for epoch in range(EPOCHS):
+        optimizer.zero_grad()  # zero the gradients from the last iteration
+
+        # Reparameterize weights and biases
+        output = snn(x.T)  # forward pass
+
+        # Compute loss
+        loss = loss_fn(output.T, y)
+        
+        # Backpropagation
+        loss.backward()
+
+        # Optimize weights
+        optimizer.step()
+
+        if epoch % 10 == 0:
+            print(f'Epoch {epoch}, Loss: {loss.item()}')
+
+    print("Training complete.")
+
+
+def test():
+    pass
+
+def main():
+    snn = StochasticNN(input_dim= INPUT_DIM, z_dim=OUTPUT_DIM)
+
+    X, Y = import_data()
+    # ground_truth_Y = Y.copy()
+    Y = torch.Tensor(one_hot_encode(Y))
+
+    train(snn, X, Y)
+
+    
+
+if __name__ == "__main__":
+    main()
+
---
+++ snn_testing/vanilla_snn.py
@@ -0,0 +1,94 @@
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+import numpy as np
+import matplotlib.pyplot as plt
+import torchvision
+import mlflow
+import os
+
+
+class StochasticNN(nn.Module):
+
+    def __init__(self, input_dim, z_dim):
+        super(StochasticNN, self).__init__()
+
+        """
+        Stochastic Neural network for Iris data from data directory with 4 input features
+        used for classification for 3 labels. 
+        """
+
+        # 4 -> 20 -> 20 -> 3
+
+        # input layer
+        self.weights_mean_ipl = nn.Parameter(torch.Tensor(20, input_dim))
+        self.weights_logvar_ipl = nn.Parameter(torch.Tensor(20, input_dim))
+        self.bias_mean_ipl = nn.Parameter(torch.Tensor(20))
+        self.bias_logvar_l1_ipl = nn.Parameter(torch.Tensor(20))
+
+        # layer 1
+        self.weights_mean_l1 = nn.Parameter(torch.Tensor(20, 20))
+        self.weights_logvar_l1 = nn.Parameter(torch.Tensor(20, 20))
+        self.bias_mean_l1 = nn.Parameter(torch.Tensor(20))
+        self.bias_logvar_l1 = nn.Parameter(torch.Tensor(20))
+
+        # output layer
+        self.weights_mean_opl = nn.Parameter(torch.Tensor(z_dim, 20))
+        self.weights_logvar_opl = nn.Parameter(torch.Tensor(z_dim, 20))
+        self.bias_mean_opl = nn.Parameter(torch.Tensor(z_dim))
+        self.bias_logvar_opl = nn.Parameter(torch.Tensor(z_dim))
+
+        self.initialize_parameters()        # weights and biases are initialized to a certain value
+
+
+    def initialize_parameters(self):
+        # Initialize means with a normal distribution and standard deviations with a small positive constant
+        # Input layer initialization (weights and biases)
+        init.kaiming_normal_(self.weights_mean_ipl, mode='fan_in', nonlinearity='relu')
+        init.constant_(self.weights_logvar_ipl, 0.1)  # small positive std deviation for stochasticity
+        init.constant_(self.bias_mean_ipl, 0)      # bias can be initialized to 0 or small value
+        init.constant_(self.bias_logvar_l1_ipl, 0.1)  # small positive std deviation for stochasticity
+
+        # Hidden layer 1 initialization (weights and biases)
+        init.kaiming_normal_(self.weights_mean_l1, mode='fan_in', nonlinearity='relu')
+        init.constant_(self.weights_logvar_l1, 0.1)  
+        init.constant_(self.bias_mean_l1, 0)      
+        init.constant_(self.bias_logvar_l1, 0.1)     
+
+        # Output layer initialization (weights and biases)
+        init.kaiming_normal_(self.weights_mean_opl, mode='fan_in', nonlinearity='relu')
+        init.constant_(self.weights_logvar_opl, 0.1)  
+        init.constant_(self.bias_mean_opl, 0)      
+        init.constant_(self.bias_logvar_opl, 0.1)     
+
+
+    def reparameterization_trick(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+
+        eps = torch.randn_like(std)
+
+        z = mu + eps * std
+
+        return z
+
+    def forward(self, x):
+        Softplus = torch.nn.Softplus() 
+        # Input Layer
+        weights_ipl = self.reparameterization_trick(self.weights_mean_ipl, self.weights_logvar_ipl)
+        bias_ipl = self.reparameterization_trick(self.bias_mean_ipl, self.bias_logvar_l1_ipl)
+        x = torch.matmul(weights_ipl, x) + bias_ipl.unsqueeze(dim=1)
+        x =  Softplus(x)
+
+        # Hidden Layer 1
+        weights_l1 = self.reparameterization_trick(self.weights_mean_l1, self.weights_logvar_l1)
+        bias_l1 = self.reparameterization_trick(self.bias_mean_l1, self.bias_logvar_l1)
+        x = torch.matmul(weights_l1, x) + bias_l1.unsqueeze(dim=1)
+        x = Softplus(x)  
+
+        # Output Layer
+        weights_opl = self.reparameterization_trick(self.weights_mean_opl, self.weights_logvar_opl)
+        bias_opl = self.reparameterization_trick(self.bias_mean_opl, self.bias_logvar_opl)
+        x = torch.matmul(weights_opl, x) + bias_opl.unsqueeze(dim=1)
+        x = torch.softmax(x, dim=-1)
+
+        return x
