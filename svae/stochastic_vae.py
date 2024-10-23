45ac6b343e8695d15cc2bff8c243318341e3126b
Shounak Desai <sd9649@rit.edu>
2024-10-23 12:03:57 -0400

    Loss function added

--- svae/stochastic_vae.py
+++ svae/stochastic_vae.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
-import torch.nn.init as init
-import numpy as np
-import matplotlib.pyplot as plt
+from training_config import LATENT_DIM
 
 
 class Stochastic_VAE(nn.Module):
@@ -13,6 +11,32 @@ class Stochastic_VAE(nn.Module):
         self.encoder = encoder
         self.decoder = decoder
 
+    def reparameterize(self, mu_z, logvar_z):
 
-    def loss(self):
-        pass
+        """Sample from the approximate posterior using the reparameterization trick."""
+
+        std = torch.exp(0.5 * logvar_z)
+        eps = torch.randn_like(std)
+
+        z = mu_z + eps * std
+
+        return z
+
+
+    def loss(self, x):
+        mu_z, logvar_z = self.encoder(x)
+
+        z = self.reparameterize(mu_z, logvar_z)
+        z = z.view((-1, LATENT_DIM))
+
+        x_recon = self.decoder(z)
+
+        reconstruction_term = self.decoder.log_likelihood(x, x_recon).sum()
+        # print(reconstruction_term.shape)
+        kl_term = self.encoder.kl(mu_z, logvar_z)                               #shape is []
+        # elbo = reconstruction_term - kl_term
+        loss =  kl_term - reconstruction_term
+
+        return loss.sum(), kl_term, reconstruction_term
+    
+    
