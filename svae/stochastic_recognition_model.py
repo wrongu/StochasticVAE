c96189473dea3ad48d4fe08f8f64a00cd2a5c90c
Shounak Desai <sd9649@rit.edu>
2024-10-25 14:33:55 -0400

    Network bug solved - forward();return still not fixed

--- svae/stochastic_recognition_model.py
+++ svae/stochastic_recognition_model.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.nn.init as init
 from training_config import PLAN
 
-
 class Stochastic_Recognition_NN(nn.Module):
 
     def __init__(self, input_dim, z_dim, user_input_logvar = -2.5):
@@ -11,65 +10,76 @@ class Stochastic_Recognition_NN(nn.Module):
         
         self.user_input_logvar = user_input_logvar
 
-        for plan_idx in range(len(PLAN)):
+        # Regular Python lists instead of nn.ModuleList
+        self.weights_mean = []
+        self.weights_logvar = []
+        self.bias_mean = []
+        self.bias_logvar = []
 
-            #input layer
+        for plan_idx in range(len(PLAN)+1):
+
+            # Input layer
             if plan_idx == 0:
-                self.weights_mean = nn.ModuleList().append(nn.Parameter(torch.Tensor(PLAN[plan_idx], input_dim)))
-                self.weights_logvar = nn.ModuleList().append(nn.Parameter(torch.Tensor(PLAN[plan_idx], input_dim)))
-                
-                self.bias_mean = nn.ModuleList().append(nn.Parameter(torch.Tensor(PLAN[plan_idx])))
-                self.bias_logvar = nn.ModuleList().append(nn.Parameter(torch.Tensor(PLAN[plan_idx])))
+                w_mean = nn.Parameter(torch.Tensor(PLAN[plan_idx], input_dim))
+                w_logvar = nn.Parameter(torch.Tensor(PLAN[plan_idx], input_dim))
+                b_mean = nn.Parameter(torch.Tensor(PLAN[plan_idx]))
+                b_logvar = nn.Parameter(torch.Tensor(PLAN[plan_idx]))
 
-            #output layer
-            elif plan_idx == len(PLAN) - 1:
-                self.weights_mean.append(nn.Parameter(torch.Tensor(z_dim, PLAN[plan_idx])))
-                self.weights_logvar.append(nn.Parameter(torch.Tensor(z_dim, PLAN[plan_idx])))
+            # Output layer
+            elif plan_idx == len(PLAN):
+                w_mean = nn.Parameter(torch.Tensor(z_dim, PLAN[plan_idx-1]))
+                w_logvar = nn.Parameter(torch.Tensor(z_dim, PLAN[plan_idx-1]))
+                b_mean = nn.Parameter(torch.Tensor(z_dim))
+                b_logvar = nn.Parameter(torch.Tensor(z_dim))
 
-                self.bias_mean.append(nn.Parameter(torch.Tensor(z_dim)))
-                self.bias_logvar.append(nn.Parameter(torch.Tensor(z_dim)))
-            
+            # Hidden layers
             else:
-                self.weights_mean.append(nn.Parameter(torch.Tensor(PLAN[plan_idx], PLAN[plan_idx - 1])))
-                self.weights_logvar.append(nn.Parameter(torch.Tensor(PLAN[plan_idx], PLAN[plan_idx - 1])))
+                w_mean = nn.Parameter(torch.Tensor(PLAN[plan_idx], PLAN[plan_idx - 1]))
+                w_logvar = nn.Parameter(torch.Tensor(PLAN[plan_idx], PLAN[plan_idx - 1]))
+                b_mean = nn.Parameter(torch.Tensor(PLAN[plan_idx]))
+                b_logvar = nn.Parameter(torch.Tensor(PLAN[plan_idx]))
 
-                self.bias_mean.append(nn.Parameter(torch.Tensor(PLAN[plan_idx])))
-                self.bias_logvar.append(nn.Parameter(torch.Tensor(PLAN[plan_idx])))
+            # Register parameters
+            self.register_parameter(f"weights_mean_{plan_idx}", w_mean)
+            self.register_parameter(f"weights_logvar_{plan_idx}", w_logvar)
+            self.register_parameter(f"bias_mean_{plan_idx}", b_mean)
+            self.register_parameter(f"bias_logvar_{plan_idx}", b_logvar)
+
+            # Append to lists
+            self.weights_mean.append(w_mean)
+            self.weights_logvar.append(w_logvar)
+            self.bias_mean.append(b_mean)
+            self.bias_logvar.append(b_logvar)
 
         self.initialize_parameters()
 
-
     def initialize_parameters(self):
-
         for i, layer in enumerate(self.weights_mean):
             init.kaiming_normal_(layer, mode='fan_in', nonlinearity='relu')
-
         for i, layer in enumerate(self.weights_logvar):
-            init.constant_(layer,self.user_input_logvar) 
-
+            init.constant_(layer, self.user_input_logvar)
         for i, layer in enumerate(self.bias_mean):
             init.constant_(layer, 0)
-
         for i, layer in enumerate(self.bias_logvar):
             init.constant_(layer, self.user_input_logvar)
 
     def reparameterization_trick(self, mu, logvar):
         std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
-
-        z = mu + eps * std
-
-        return z
+        return mu + eps * std
     
     def forward(self, x):
-        Softplus = torch.nn.Softplus()
 
         for i in range(len(self.weights_mean)):
             weights_ipl = self.reparameterization_trick(self.weights_mean[i], self.weights_logvar[i])
             bias_ipl = self.reparameterization_trick(self.bias_mean[i], self.bias_logvar[i])
-            x = torch.matmul(weights_ipl, x) + bias_ipl.unsqueeze(dim=1)  
-            
-            if i == len(self.weights_mean) - 1:                 #output layer
-                x =  Softplus(x)
+            x = torch.matmul(x, weights_ipl.T) + bias_ipl.view(1, -1)
 
-        return x                
+            if i == len(self.weights_mean) - 1:  # Output layer
+                Softplus = nn.Softplus()
+                x = Softplus(x)
+            else:
+                ReLU = nn.ReLU()
+                x = ReLU(x)
+
+        return x
