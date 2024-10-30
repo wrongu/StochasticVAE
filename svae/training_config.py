4144da95c816cc66b76ab0b8ea52c798b318736d
Shounak Desai <sd9649@rit.edu>
2024-10-30 11:53:39 -0400

    First Commit

--- svae/training_config.py
+++ svae/training_config.py
@@ -5,6 +5,6 @@ PLAN_DECODER = [50, 100, 300, 500]
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 LEARNING_RATE = 1e-4
-EPOCHS = 100
+EPOCHS = 10
 BATCH_SIZE = 128
 LATENT_DIM = 20
